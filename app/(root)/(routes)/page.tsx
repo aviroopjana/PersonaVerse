e220f545568cdaf0be6a7f659480673adeaf9b9c
Aviroop Jana <aviroopjana@gmail.com>
2023-08-28 02:56:21 +0530

    feat: rendering companion list ✅

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,7 +2,12 @@ import { Categories } from "@/components/categories";
 import { Companions } from "@/components/companions";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prisma_db";
+import {createClient} from "@supabase/supabase-js";
 
+// const supabaseUrl = "https://luivnfiftgbnwdhsdejb.supabase.co";
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1aXZuZmlmdGdibndkaHNkZWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0MTI2ODIsImV4cCI6MjAwNjk4ODY4Mn0.-hglwGjQMIaJ8dYhm8llrCoXD_UVyJX3ZwfZ_v8-1PA";
+
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
 interface RootPageProps {
     searchParams: {
@@ -36,13 +41,25 @@ const RootPage = async({
 
     const categories = await prismadb.category.findMany();
 
+    // async function searchFullText(query: string) {
+    //     const { data, error } = await supabase.rpc('full_text_search', { query });
+    //     if (error) {
+    //       console.error(error);
+    //       return null;
+    //     }
+    //     return data;
+    //   }
+
+    //   const searchResults = await searchFullText(searchParams.name);
+
     return (
         <div className="h-full p-4 ml-4 space-y-2" >
             <SearchInput/>
             <Categories data={categories} />    
-            <Companions data={data} />       
+            <Companions data={data}/>       
         </div>
     );
 }
  
 export default RootPage;
+
--- components/companions.tsx
+++ components/companions.tsx
@@ -1,12 +1,15 @@
 import { Companion } from "@prisma/client"
 import Image from "next/image";
+import { Card, CardFooter, CardHeader } from "./ui/card";
+import Link from "next/link";
+import { MessagesSquare } from "lucide-react";
 
 interface CompanionProps {
     data: (Companion & {
         _count: {
             messages: number,
         }
-    })[];
+    })[]
 }
 
 export const Companions = ({
@@ -24,13 +27,55 @@ export const Companions = ({
                         src="/empty.png"
                     />
                 </div>
+                <p className="text-sm text-muted-foreground">
+                    No companions yet!
+                </p>                
             </div>
         )
     }
 
     return(
-        <div>
-            Companions!
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10" >
+           {data.map((item) => (
+             <Card
+                key={item.id}
+                className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0"
+             >
+                <Link
+                    href={`/chat/${item.id}`}
+                >
+                    <CardHeader
+                        className="flex items-center justify-center text-center text-muted-foreground"
+                    >
+                        <div className="relative w-32 h-32">
+                            <Image
+                                alt="Companion"
+                                src={item.src}
+                                fill
+                                className="rounded-xl object-cover"
+                            />
+                        </div>
+                        <p className="font-bold">
+                            {item.name}
+                        </p>
+                        <p className="text-xs">
+                            {item.description}
+                        </p>
+                    </CardHeader>
+                    <CardFooter 
+                        className="flex items-center justify-between text-xs text-muted-foreground"
+                    >
+                        <p className="lowercase">
+                            @{item.userName}
+                        </p>
+                        <div className="flex items-center">
+                            <MessagesSquare className="w-3 h-3 mr-1" />
+                            {item._count.messages}
+                        </div>
+                    </CardFooter>
+                </Link>
+            </Card>
+           ))}
         </div>
     )
 }
