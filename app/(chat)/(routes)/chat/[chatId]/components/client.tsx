20790209da8b525cddd8112aaba50d6f58c39fb1
Aviroop Jana <aviroopjana@gmail.com>
2023-09-20 21:53:20 +0530

    fix: companion response error

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,78 +1,76 @@
 "use client";
 
-import { ChatHeader } from "@/components/chat-header";
 import { useCompletion } from "ai/react";
-
+import { FormEvent, useState } from "react";
 import { Companion, Message } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+
 import { ChatForm } from "@/components/chat-form";
+import { ChatHeader } from "@/components/chat-header";
 import { ChatMessages } from "@/components/chat-messages";
 import { ChatMessageProps } from "@/components/chat-message";
 
-
 interface ChatClientProps {
-    companion: Companion & {
-        messages: Message[];
-        _count: {
-            messages: number;
-        };
-    };
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
+    }
+  };
 };
 
 export const ChatClient = ({
-    companion
-}:ChatClientProps) => {
+  companion,
+}: ChatClientProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatMessageProps[]>(companion.messages);
+  
+  const {
+    input,
+    isLoading,
+    handleInputChange,
+    handleSubmit,
+    setInput,
+  } = useCompletion({
+    api: `/api/chat/${companion.id}`,
+    onFinish(_prompt, completion) {
+      const systemMessage: ChatMessageProps = {
+        role: "system",
+        content: completion
+      };
 
-    const router = useRouter();
-    const [messages, setMessages] = useState<ChatMessageProps[]>(companion.messages);
+      setMessages((current) => [...current, systemMessage]);
+      setInput("");
 
-    const {
-        input,
-        isLoading,
-        handleInputChange,
-        handleSubmit,
-        setInput
-    } = useCompletion({
-        api: `/api/chat/${companion.id}`,
-        onFinish(prompt, completion) {
-            const systemMessage: ChatMessageProps = {
-                role: "system",
-                content: completion,
-            };
+      router.refresh();
+    },
+  });
 
-            setMessages((current) => [...current, systemMessage]);
-            setInput("");
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    const userMessage: ChatMessageProps = {
+      role: "user",
+      content: input
+    };
 
-            router.refresh();
-        },
-    });
+    setMessages((current) => [...current, userMessage]);
 
-    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-        const userMesage: ChatMessageProps= {
-            role: "user",
-            content: input
-        }
+    handleSubmit(e);
+  }
 
-        setMessages((current) => [...current, userMesage]);
-
-        handleSubmit(e);
-    }
-
-    return (
-        <div className="flex flex-col h-full p-4 space-y-2">
-            <ChatHeader companion={companion} />
-            <ChatMessages
-                companion={companion}
-                isLoading={isLoading}
-                messages={messages}
-            />
-            <ChatForm
-                isLoading={isLoading}
-                input={input}
-                handleInputChange={handleInputChange}
-                onSubmit={onSubmit}
-            />
-        </div>
-    )
+  return (
+    <div className="flex flex-col h-full p-4 space-y-2">
+      <ChatHeader companion={companion} />
+      <ChatMessages 
+        companion={companion}
+        isLoading={isLoading}
+        messages={messages}
+      />
+      <ChatForm 
+        isLoading={isLoading} 
+        input={input} 
+        handleInputChange={handleInputChange} 
+        onSubmit={onSubmit} 
+      />
+    </div>
+   );
 }
